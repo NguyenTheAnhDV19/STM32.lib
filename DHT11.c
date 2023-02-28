782705f10327f586ffc9ae2a81493369f3a12143
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2023-02-28 20:40:07 +0700

    Update DHT11.c

--- DHT11.c
+++ DHT11.c
@@ -1,6 +1,13 @@
+/*
+	link to the datasheet is here:
+		https://html.alldatasheet.com/html-pdf/1440068/ETC/DHT11/395/6/DHT11.html
+*/
+
+
 #include "DHT11.h"
 
 void getData(DHT11* dht11,TIM_HandleTypeDef* htim){
+	//here confingure the pin B14 on Stm32 at output mode first to put it in reset mode -> signal the slave to gives feed back to master
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
 	  GPIO_InitStruct.Pin = GPIO_PIN_14;
@@ -8,21 +15,25 @@ void getData(DHT11* dht11,TIM_HandleTypeDef* htim){
 	  GPIO_InitStruct.Pull = GPIO_NOPULL;
 	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
+	
+	//put Pin14 in reset mode and delay for at least 18ms (see the datasheet page 6/9 for more information)
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
 	HAL_Delay(20);
-
+	
+	//configure Pin14 to Input_Pullup mode so the slave can pulldown the pinmode, this process takes up about 20-40ms
 		GPIO_InitStruct.Pin = GPIO_PIN_14;
 		GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
 		GPIO_InitStruct.Pull = GPIO_PULLUP;
 		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-
+	//here delay for 45 ms, wait till slave pullpin down
 	delay_us(htim, 45);
 
+	//see datasheet page 6/9 for more information 
 	getTime(htim,80,1);
 	getTime(htim,80,0);
 
+	// get data to buffer here - including humid and tempeature 
 	for(int i=0;i<5;i++){
 	    for(int j=0;j<8;j++){
 	      uint16_t time_wait =getTime(htim,50,1);
@@ -46,6 +57,7 @@ int getHumid(DHT11* dht11){
 	return dht11->buffer[0];
 }
 
+//This function used to wait till the mode of the pin meet mode, then return the total waiting time, it'll return 0 if the time is exceeded limit
 int getTime(TIM_HandleTypeDef* htim,uint8_t time, bool mode){
 	HAL_TIM_Base_Start(htim);
 	__HAL_TIM_SET_COUNTER(htim,0);
@@ -62,6 +74,7 @@ void delay_us(TIM_HandleTypeDef* htim,uint16_t us){
 	while (__HAL_TIM_GET_COUNTER(htim) < us){};
 }
 
+//see the datasheet for more information
 bool debug(DHT11* dht11)
 {
     if(dht11->buffer[0]+dht11->buffer[1]+dht11->buffer[2]+dht11->buffer[3] != dht11->buffer[4]) return false;
